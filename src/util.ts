9d0460762fb774184b751478d5bbddb501310f88
Andy Peterson <andy@ndpsoftware.com>
2023-01-18 15:35:06 -0800

    mvp image functionality

--- src/util.ts
+++ src/util.ts
@@ -28,3 +28,15 @@ export async function tableHasColumn(db: Database, table: string, column: string
   WHERE name='${column}'`))['COUNT(*)'];
 }
 
+export type MimeType = 'image/jpeg' | 'image/png' | 'image/gif'
+
+export function pathToMimeType(path: string): MimeType {
+  if (path.match(/jpe?g$/i))
+    return 'image/jpeg'
+  if (path.match(/gif$/i))
+    return 'image/gif'
+  if (path.match(/png$/i))
+    return 'image/png'
+  throw `Unknown mime type for file ${path}`
+}
+
