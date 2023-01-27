3c00158d81caf0cd86f0a8cbbde53884ca5b2112
Andy Peterson <andy@ndpsoftware.com>
2023-01-27 12:59:54 -0600

    add documentation

--- src/util/makeRetryable.ts
+++ src/util/makeRetryable.ts
@@ -1,10 +1,30 @@
-import {sleep} from "./promise";
+import {isPromise, sleep} from "./promise";
 
 interface MakeRetryableOptions {
   retryable: (count: number, exception: any) => boolean,
   delay?: number | ((count: number, exception: any) => number)
 }
 
+/**
+ * Given a function, returns a function with built-in retry capabilities.
+ * Failure for normal functions means throwing an exception, and failure
+ * for Promise/async functions means returning a rejected promise.
+ *
+ *
+ * The retry capabilities are controlled by the options provided:
+ *
+ * -  `retryable` is a function you provide that determines whether a specific
+ *    failure will retry. It receives the retry count (starting at 1), and
+ *    the exception that was thrown/rejected, and returns true or false.
+ *    Although it's possible to retry "forever" (pass `() => true`), a better
+ *    choice might be some sort of retry limit such as `(c) => c < 5`, or
+ *    based on a specific exception, such as `(c,e) => e.status === 503`.
+ *
+ * -  `delay` is how long to wait before retrying. By default this is no delay,
+ *    but you can provide a number (of milliseconds), or a function, which
+ *    could implement exponential backoff ala `(c) => Math.pow(2, c) * 1000`
+ *    or some other mechanism.
+ */
 export function makeRetryable<A extends Array<unknown>, R = void>(targetFn: (...args: A) => R, options: MakeRetryableOptions): (...args: A) => R {
 
   let retryCount = 0
@@ -43,7 +63,3 @@ export function makeRetryable<A extends Array<unknown>, R = void>(targetFn: (...
   }
 }
 
-
-function isPromise<T = any>(obj: any): obj is T extends (...args: any[]) => any ? Promise<Awaited<ReturnType<T>>> : never {
-  return !!obj && typeof obj.then === 'function';
-}
--- src/util/promise.ts
+++ src/util/promise.ts
@@ -1,6 +1,15 @@
 /*
-A standard "sleep" method implemented via setTimeout/sleep.
+A "sleep" method implemented via setTimeout/sleep.
  */
 export function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+/*
+An `isPromise` detector that gets the type of the promise return type correct, when returning `true`.
+ */
+export function isPromise<T = any>(obj: any):
+  obj is T extends (...args: any[]) => any ? Promise<Awaited<T>> : never {
+  return !!obj && typeof obj.then === 'function';
 }
