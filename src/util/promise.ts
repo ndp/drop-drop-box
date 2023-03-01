73bd0694b8045f082079d165199f36804819be7f
Andy Peterson <andy@ndpsoftware.com>
2023-02-28 18:59:27 -0800

    cleanup

--- src/util/promise.ts
+++ src/util/promise.ts
@@ -1,9 +1,8 @@
 /*
 A "sleep" method implemented via setTimeout/sleep.
  */
-export function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+export const sleep = (ms = 0): Promise<void> =>
+  new Promise(r => setTimeout(r, ms));
 
 
 /*
